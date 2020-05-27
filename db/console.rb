32f9c526d49af8b93178952f8793af5b0b827514
Ahmed Hamed <tunashark@gmail.com>
2020-05-27 16:05:00 +0100

    Added save method to Album

--- db/console.rb
+++ db/console.rb
@@ -9,3 +9,5 @@ album1 = Album.new( {'title' => 'Love me tender', 'genre' => 'Rock'} )
 album2 = Album.new( {'title' => 'Ring of fire', 'genre' => 'Rock'} )
 album1.save()
 album2.save()
+
+
--- models/album.rb
+++ models/album.rb
@@ -10,6 +10,21 @@ class Album
         # @artist_id = options['artist_id'].to_i()
     end
 
+    def save()
+        sql = " 
+        INSERT INTO albums (
+            title,
+            genre
+        )
+        VALUES
+        (
+            $1, $2
+        )
+        RETURNING id"
+        values = [@title, @genre]
+        @id = SqlRunner.run(sql, values)[0]['id'].to_i
+    end
+        
     
 
 end
