3c545f96e321ab9616be97a0b291f86603a6761f
Ahmed Hamed <tunashark@gmail.com>
2020-05-27 15:44:40 +0100

    Created Album class

---
+++ models/album.rb
@@ -0,0 +1,14 @@
+class Album
+
+    attr_accessor :name, :title, :genre
+    attr_reader :id, :artist_id
+
+    def initialize(options)
+        @name = options['name']
+        @title = options['title']
+        @genre = options['genre']
+        @id = options['id'].to_i() if options['id']
+        # @artist_id = options['artist_id'].to_i()
+    end
+
+end
