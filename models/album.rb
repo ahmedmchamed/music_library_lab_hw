8ca273c9a6bf02122dd216be8c63603d91243360
Ahmed Hamed <tunashark@gmail.com>
2020-05-27 19:02:21 +0100

    removed incorrect syntax from instance delete

--- models/album.rb
+++ models/album.rb
@@ -28,7 +28,7 @@ class Album
     end
 
     def delete()
-        sql = "DELETE * FROM albums WHERE id = $1"
+        sql = "DELETE FROM albums WHERE id = $1;"
         values = [@id]
         SqlRunner.run(sql, values)
     end
