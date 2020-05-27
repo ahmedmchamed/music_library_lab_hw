6c3f75bed629fab5fc2377ec32542f8cc69eca9e
Ahmed Hamed <tunashark@gmail.com>
2020-05-27 19:00:31 +0100

    Added instance delete to Artist

--- models/artist.rb
+++ models/artist.rb
@@ -24,6 +24,12 @@ class Artist
         @id = SqlRunner.run(sql, values)[0]['id'].to_i()
     end
 
+    def delete()
+        sql = "DELETE * FROM artists WHERE id = $1"
+        values = [@id]
+        SqlRunner.run(sql, values)
+    end
+
     def self.all()
         sql = "SELECT * FROM artists;"
         artist_hashes = SqlRunner.run(sql)
